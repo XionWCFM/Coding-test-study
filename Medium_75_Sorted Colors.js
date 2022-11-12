d1092c88a802c54ef87042ad2bd1bbd3b177564a
5kdk <tylerk1121@gmail.com>
2022-11-12 22:02:53 +0900

    Medium / 75 Sorted Colors

--- Medium_75_Sorted Colors.js
+++ Medium_75_Sorted Colors.js
@@ -2,7 +2,7 @@
 https://leetcode.com/problems/sort-colors/
 문제 Medium 75 Sort Colors
 
-빨간색, 흰색 또는 파란색으로 채색된 n개의 객체가 있는 배열 번호가 주어지면
+빨간색, 흰색 또는 파란색으로 채색된 n개의 객체가 있는 배열 nums가 주어지면
 
 동일한 색상의 객체가 인접하도록 해당 위치에서 정렬하고 색상은 빨간색, 흰색, 파란색으로 지정합니다.
 
@@ -32,16 +32,19 @@ exam 2 :
 // [1, 0, 2, 1, 1, 2, 0]
 // start point         end point
 // 0일경우 start point과 바꾸고 start point +1 idx + 1
-// 2일경우 end point와 바꾸고 end point + 1 idx는 이동 no (바꾼 end 숫자가 0이나 1일수도 있으니)
+// 2일경우 end point와 바꾸고 end point - 1 idx는 이동 no (바꾼 end 숫자가 0이나 1일수도 있으니)
 // 1일경우 일단 무시 idx 이동
 
-// [1, 0, 2, 1, 1, 2, 0]  start=0, idx=0, end=6        :  idx +1
-// [0, 1, 2, 1, 1, 2, 0]  start=0, idx=1, end=6        :  start +1, idx +1
-// [0, 1, 0, 1, 1, 2, 2]  start=1, idx=2, end=6        :  end -1
-// [0, 1, 0, 1, 1, 2, 2]  start=1, idx=2, end=5        :  start +1 idx +1
-// [0, 0, 1, 1, 1, 2, 2]  start=2, idx=3, end=5        :  idx +1
-// [0, 0, 1, 1, 1, 2, 2]  start=2, idx=4, end=5        :  idx +1
-// [0, 0, 1, 1, 1, 2, 2]  start=2, idx=5, end=5        :  break
+// [1✔, 0, 2, 1, 1, 2, 0]  start=0, idx=0, end=6      ->  idx +1
+// [1, 0✔, 2, 1, 1, 2, 0]  start=0, idx=1, end=6      ->  start +1, idx +1
+// [0, 1, 2✔, 1, 1, 2, 0]  start=1, idx=2, end=6      ->  end - 1
+// [0, 1, 0✔, 1, 1, 2, 2]  start=1, idx=2, end=5      ->  idx +1
+// [0, 0, 1, 1✔, 1, 2, 2]  start=1, idx=3, end=5      ->  idx +1
+// [0, 0, 1, 1, 1✔, 2, 2]  start=1, idx=3, end=5      ->  idx +1
+// [0, 0, 1, 1, 1, 2✔, 2]  start=1, idx=3, end=5      ->  idx +1
+
+
+
 
 
 var sortColors = function (nums) {
